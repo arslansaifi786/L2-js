6efb08a322e0977694369ae43a86c545f2c2ca79
arslan <saifiarslaan01@gmail.com>
2024-09-25 10:50:44 -0700

    some question

---
+++ array-qu.js
@@ -0,0 +1,73 @@
+let arr=[10,20,30,50,60]
+
+let end=arr.length-1
+let st=0
+
+for(st<end;st++;end--){
+    console.log(arr);
+    
+}
+
+
+console.log("1"-1);
+
+
+
+let str="I love javascript and hate it at the same time."
+function checklogsrting(params) {
+
+    if (str.strim().length===0) {
+        return false;
+
+    }
+    
+//     let str
+    
+}
+
+checklogsrting( )
+
+
+
+function checksamestr(str) {
+
+
+let str="my name is vikas my name is vikas"
+
+    let word=str.split(" ")
+
+    // let fre="vikas"
+    let count=0
+
+    for(let i=0;i<word.length;i++){
+
+        if("vikas"==word[i]){
+            count=i
+            break
+        }
+
+    }
+
+    console.log(count);
+
+    
+
+
+
+
+
+
+
+
+    let str="vikas is name my"
+
+    let word1=str.split(" ")
+     let st=0
+     let end=word.length-1
+    for(;st<end;st++,end-- ){
+        let temp=word[st];
+        word[st]=word[end]
+        word[end]=temp
+    }
+    console.log(word1);
+}
